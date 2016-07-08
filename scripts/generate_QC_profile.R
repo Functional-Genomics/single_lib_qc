5f4c5fd8a637ccaa28126454f6ee123c0e9e1f33
Egor Revkov <revkov.egor@gmail.com>
2016-07-08 16:09:07 +0100

    Update generate_QC_profile.R

--- scripts/generate_QC_profile.R
+++ scripts/generate_QC_profile.R
@@ -47,7 +47,7 @@ GenerateQCProfile <- function (path_to_directory, index) {
   files_paths <- unique(files_paths) #getting rid of the duplicates (check for another way?)
   
   #IMPORTANT: checks for the library's integrity (there should be only 5 files)
-  if (length(files_paths)!=5){
+  if (length(files_paths) != 5) {
     cat(index, " ", "ERROR! incomplete library:")
     for (type in types_of_files) {
       if (length((grep(paste0(type, "($|\\s)"), files_paths))) == 0) {
@@ -55,13 +55,15 @@ GenerateQCProfile <- function (path_to_directory, index) {
       }
     }
     cat("\n")
-    q(status=1)
+    q(status = 1)
   }
   
   
   #reading data from the file as a list of dataframes (specific, as there are always only 5 files that we need to check)
-  reads_from_info <- read.table(files_paths[grep("info", files_paths)], comment.char = " ")
-  reads_from_stats <- lapply(files_paths[-grep("info", files_paths)], fread, header=FALSE)
+  reads_from_info <-
+    read.table(files_paths[grep("info", files_paths)], comment.char = " ")
+  reads_from_stats <-
+    lapply(files_paths[-grep("info", files_paths)], fread, header = FALSE)
   
   #getting the nreads and rs data from the info 
   info_dataframe <- GetRsNreads (reads_from_info)
@@ -71,10 +73,14 @@ GenerateQCProfile <- function (path_to_directory, index) {
   reads_from_stats[[grep("iRAP", reads_from_stats)]] <- convert_time_list (reads_from_stats) #converting time/memory dataframe to a 2-column one, leaving memory (.time dataframe)
   
   #adding prefixes
-  reads_from_stats[[grep("Exons|Introns", reads_from_stats)]]$V1 <- paste0("GENE.STATS_",  reads_from_stats[[grep("Exons|Introns", reads_from_stats)]]$V1)
-  reads_from_stats[[grep("gene", reads_from_stats)]]$V1 <- paste0("STATS_",  reads_from_stats[[grep("gene", reads_from_stats)]]$V1)
-  reads_from_stats[[grep("entries", reads_from_stats)]]$V1 <- paste0("STATS.CSV_",  reads_from_stats[[grep("entries", reads_from_stats)]]$V1)
-  reads_from_stats[[grep("iRAP", reads_from_stats)]]$V1 <- paste0("TIME_",  reads_from_stats[[grep("iRAP", reads_from_stats)]]$V1)
+  reads_from_stats[[grep("Exons|Introns", reads_from_stats)]]$V1 <-
+    paste0("GENE.STATS_",  reads_from_stats[[grep("Exons|Introns", reads_from_stats)]]$V1)
+  reads_from_stats[[grep("gene", reads_from_stats)]]$V1 <-
+    paste0("STATS_",  reads_from_stats[[grep("gene", reads_from_stats)]]$V1)
+  reads_from_stats[[grep("entries", reads_from_stats)]]$V1 <-
+    paste0("STATS.CSV_",  reads_from_stats[[grep("entries", reads_from_stats)]]$V1)
+  reads_from_stats[[grep("iRAP", reads_from_stats)]]$V1 <-
+    paste0("TIME_",  reads_from_stats[[grep("iRAP", reads_from_stats)]]$V1)
   
   #converting list of the dataframes to a single dataframe
   stats_dataframe <- rbindlist(reads_from_stats)
@@ -87,7 +93,7 @@ GenerateQCProfile <- function (path_to_directory, index) {
   names(profile_vector) <- bind_rows(info_dataframe, stats_dataframe)$V1
   
   #creating profile dataframe
-  profile_dataframe<- transform(profile_vector, as.numeric())
+  profile_dataframe <- transform(profile_vector, as.numeric())
   colnames(profile_dataframe) <- index
   
   #transposing profile dataframe
@@ -105,12 +111,14 @@ GenerateQCProfile <- function (path_to_directory, index) {
 #gets rs and nreads from the list with data from .info, returns a dataframe with columns V1 and V2
 GetRsNreads <- function (reads_from_info) {
   
-  sample <- str_split_fixed(reads_from_info$V1, "#", n=2)
+  sample <- str_split_fixed(reads_from_info$V1, "#", n = 2)
   nreads <- strsplit(sample[grep("nreads", sample)], "=")
-  rs <- strsplit(sample[grep("rs", sample)], "=")[[2]] #!TOO SPECIFIC CODE/REDO
+  
+  rs <-strsplit(sample[grep("rs", sample)], "=")[[2]] #TODO:think of a less specific way
   rs_nreads_list <- c(nreads, rs)
   
-  rs_nreads_dataframe <- data.frame(matrix(unlist(rs_nreads_list), nrow=2, byrow=T),stringsAsFactors=FALSE)
+  rs_nreads_dataframe <-
+    data.frame(matrix(unlist(rs_nreads_list), nrow = 2, byrow = T), stringsAsFactors =FALSE)
   colnames(rs_nreads_dataframe) = c("V1", "V2")
   
   return(rs_nreads_dataframe)
@@ -119,8 +127,10 @@ GetRsNreads <- function (reads_from_info) {
 #converts the "time" list of the reads_from_stats to a 2-column dataframe (time+memory)
 convert_time_list <- function (reads_from_stats) {
   
-  col_memory_names <-paste0(c("fastqInfo","iRAP-QC","iRAP-Mapping","iRAP-Quant","iRAP-Mapping-QC","iRAP-CRAM"), "_memory")
+  col_memory_names <- 
+    paste0(c("fastqInfo","iRAP-QC","iRAP-Mapping","iRAP-Quant","iRAP-Mapping-QC","iRAP-CRAM"), "_memory")
   col_memory_values <- reads_from_stats[[grep("iRAP", reads_from_stats)]]$V4
+  
   memory_dataframe <- data.frame (col_memory_names, col_memory_values)
   colnames(memory_dataframe) <- c("V1", "V2")
   
@@ -149,8 +159,9 @@ DropUnnecessaryColumns <- function (data_frame) {
   unnecessary_columns_list <- list ("source", "IG", "TR")
   
   for (name in unnecessary_columns_list) {
-    if ( length(grep(name, colnames(data_frame))) != 0 ) 
-      data_frame <- data_frame[,-grep(name, colnames(data_frame))] #source column
+    if (length(grep(name, colnames(data_frame))) != 0)
+      data_frame <-
+        data_frame[, -grep(name, colnames(data_frame))] #source column
   }
   
   return (data_frame)
