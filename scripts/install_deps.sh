14e4cf4311a7d48aada791876657250f07217112
Nuno Fonseca <nf@ebi-005.ebi.ac.uk>
2016-07-18 14:02:03 +0100

    dependencies: R packages

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 # Script to install all software needed
+
+R --no-save <<EOF
+source("http://bioconductor.org/biocLite.R")
+
+for (p in c("dplyr", "data.table", "dtplyr", "stringr") ) {
+   biocLite(p)
+}
+EOF
