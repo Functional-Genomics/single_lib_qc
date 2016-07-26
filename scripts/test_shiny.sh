96d8283010b7e16ac16f96ca5055424eb23d5ea3
egorrevkov <revkov.egor@gmail.com>
2016-07-26 15:54:19 +0100

    fixes to test_shiny script

--- scripts/test_shiny.sh
+++ scripts/test_shiny.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-path_to_matrix=`echo $PWD/profiles_data/extended_matrix`
-Rscript -e 'library(methods); shiny::runApp("ShinyApp/", launch.browser=TRUE)' $path_to_matrix
+if [ "$#" -ne 2 ]; then
+  echo "ERROR: Incorrect amount of arguments!"
+  echo "Usage: <path_to_ShinyApp> <path_to_extended_matrix>"
+  exit 1
+fi
+
+path_to_shiny=$1
+path_to_matrix=$2
+Rscript -e "library(methods); shiny::runApp('$path_to_shiny', launch.browser=TRUE)" $path_to_matrix
 
 exit
