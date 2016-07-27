9a6b41836ca5c9a85603da028005bcf5ee131f81
egorrevkov <revkov.egor@gmail.com>
2016-07-27 11:17:24 +0100

    adding script for submitting jobs to cluster

---
+++ scripts/create_prof_clust.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+libraries_storage=$1 # where to look for directories with libraries
+profiles_storage=$2 # where to put profiles
+pattern=$3 # what directories to look for
+
+L=`ls -d $libraries_storage/$pattern/`
+
+for dir in $L
+do
+  bsub gen_QC_profile_single.sh $dir $profiles_storage
+done
+
+exit
