4b16b61db66fbff6babb8a8ee15ead4a1ea943d3
Nuno Fonseca <firstname.lastname@gmail.com>
2016-08-17 21:56:23 +0100

    Add a few checks

--- scripts/test_shiny_plotly.sh
+++ scripts/test_shiny_plotly.sh
@@ -3,7 +3,18 @@
 set -e
 
 path_to_shiny=`pwd`/ShinyApp_Plotly
-path_to_matrix=`pwd`/profiles_data/extended_matrix
+if [ ! -e $path_to_shiny ]; then
+    echo "ERROR: unable to find $path_to_shiny"
+    exit 1
+fi
+if [ "$1-" == "-" ]; then
+    path_to_matrix=`pwd`/profiles_data/extended_matrix
+else
+    path_to_matrix=$1
+fi
+
+which Rscript >> /dev/null || ( echo "ERROR: Rscript not found" && exit 1 )
+
 Rscript -e "library(methods); shiny::runApp('$path_to_shiny', launch.browser=TRUE)" $path_to_matrix
 
 exit
