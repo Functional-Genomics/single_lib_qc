0962239ce01d0422dceee8fd631b92f3cd72b78c
egorrevkov <revkov.egor@gmail.com>
2016-08-25 16:02:45 +0100

    updates to profile generating script

--- scripts/create_prof_clust.sh
+++ scripts/create_prof_clust.sh
@@ -5,6 +5,7 @@ set -e
 libraries_storage=$1 # where to look for directories with libraries
 profiles_storage=$2 # where to put profiles
 pattern=$3 # what directories to look for
+path_to_runs_file=$4 #OPTIONAL; file, wehre to look for additional data
 
 if [ -z "$3" ]
 then
@@ -15,7 +16,7 @@ fi
 
 for dir in $L
 do
-  bsub -o $profiles_storage/stdoutput -e $profiles_storage/stderror gen_QC_profile_single.sh $dir $profiles_storage
+  bsub -o $profiles_storage/stdoutput -e $profiles_storage/stderror gen_QC_profile_single.sh $dir $profiles_storage $path_to_runs_file
 done
 
 exit
